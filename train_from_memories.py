4fb2cc64c4a76716e5fcc8ca430084f3d6acddd7
Evan Hedding <eth8180@gmail.com>
2022-03-04 15:55:54 -0800

    - made new file for the game-related object classes - fixed the loss function to work for q-values - model structure changed (I've been trying out different setups) - consolidated some code in the game_run file to make repeat sims easier

--- train_from_memories.py
+++ train_from_memories.py
@@ -19,35 +19,28 @@ class foosPong_model(tf.keras.Model):
     def __init__(self):
         super(foosPong_model, self).__init__()
         ###############################################
-        self.drop = tf.keras.layers.Dropout(0.15)
+        #self.drop = tf.keras.layers.Dropout(0.10)
         
         self.n1 = tf.keras.layers.BatchNormalization()
-        self.n2 = tf.keras.layers.BatchNormalization()
-        #self.n3 = tf.keras.layers.BatchNormalization()
-        #self.n4 = tf.keras.layers.BatchNormalization()
         
-        self.d1 = tf.keras.layers.Dense(100)
-#        self.d2 = tf.keras.layers.Dense(200, activation='relu')
-#        self.d3 = tf.keras.layers.Dense(200)
-        self.d4 = tf.keras.layers.Dense(100, activation='relu')
-        self.d5 = tf.keras.layers.Dense(50)
-        self.d6 = tf.keras.layers.Dense(2, activation='softmax')
+        self.d1 = tf.keras.layers.Dense(48, activation='relu')
+        self.d2 = tf.keras.layers.Dense(96, activation='relu')
+        self.d3 = tf.keras.layers.Dense(192, activation='relu')
+        self.d4 = tf.keras.layers.Dense(96, activation='relu')
+        self.d5 = tf.keras.layers.Dense(48, activation='relu')
+        
+        # size 4, so that each teammate has action space of (up, down)
+        # output here is Q value for each possible action for each teammate, which gets added together in loss function for total max q-value
+        self.d6 = tf.keras.layers.Dense(4, activation='relu')
         
         ###############################################
         
     def call(self, x):
         x = self.d1(x)
-        #x = self.drop(x)
-        
-#        x = self.d2(x)
-#        x = self.n1(x)
-#
-#        x = self.d3(x)
-#        #x = self.drop(x)
-        
+        x = self.d2(x)
+        x = self.d3(x)
+        x = self.n1(x)
         x = self.d4(x)
-        #x = self.n2(x)
-        
         x = self.d5(x)
         return self.d6(x)
 
@@ -58,33 +51,52 @@ class foosPong_model(tf.keras.Model):
 #    else:
 #        return 0
 
-def loss(curr_output, action, reward):
+def loss(curr_output, action, reward, target_output):
+    gamma = 0.8
+
     #curr_action = tf.math.argmax(curr_output, 1)
+#    curr_output = tf.round(curr_output)
+#    target_output = tf.round(target_output)
     
-    Q1 = curr_output[:,0] + curr_output[:,1]
-    Q2 = 0.99*(np.random.random() + np.random.random()) + reward[:,0]
+    Q1 = tf.gather(curr_output[:, 0:2], tf.math.argmax(curr_output[:, 0:2], 1), axis=1) + tf.gather(curr_output[:, 2:4], tf.math.argmax(curr_output[:, 2:4], 1), axis=1)
     
-    loss = tf.keras.losses.MSE(Q1, Q2)
+    Q2 = tf.gather(target_output[:, 0:2], tf.math.argmax(target_output[:, 0:2], 1), axis=1) + tf.gather(target_output[:, 2:4], tf.math.argmax(target_output[:, 2:4], 1), axis=1)
+    
+    y = gamma*Q2 + reward[:,0]
+    
+    loss = tf.keras.losses.MSE(Q1, y)
     return loss
     
 def train_nn(memories, hasPrevModel=False):
+#################################################
+### Tune these parameters for better training
     lr = 0.00001
-    epochs = 100
-    batch_size = 1000
+    epochs = 10000
+    batch_size = 100
+  #################################################  
+
     
     optimizer = tf.keras.optimizers.Adam(learning_rate=lr)
     train_loss = tf.keras.metrics.Mean(name='train_loss')
+    
     curr_model = foosPong_model()
+    prev_model = foosPong_model()
     
     if hasPrevModel:
-        prev_model = foosPong_model()
-        prev_model.load_weights('foosPong_model_weights_0')
+        curr_model.load_weights('./trained_weights/foosPong_model_v0')
+        prev_model.load_weights('./trained_weights/foosPong_model_v0')
     
     
     @tf.function
     def train(train_data):
+        steps = 0
         for tensor in train_data:
+#            #updates the target model after so many batches
+#            if steps % 1000 == 0:
+#                prev_model = curr_model
+                
             train_step(tensor)
+            steps = steps + 1
 
 
     @tf.function
@@ -95,35 +107,42 @@ def train_nn(memories, hasPrevModel=False):
         next_state = tensor[:, 28:]
         
         with tf.GradientTape() as tape:
-            current_loss = loss(curr_model(state), action, reward)
+            current_loss = loss(curr_model(state), action, reward, prev_model(next_state))
 
         grad = tape.gradient(current_loss, curr_model.trainable_variables)
         optimizer.apply_gradients(zip(grad, curr_model.trainable_variables))
         train_loss(current_loss)
 
     train_data = []
-    data_size = 100000
-    for i in range(data_size):
-        idx = int(np.floor(np.random.random()*memories[0].shape[0]))
-        
-        state = memories[0][idx,:]
-        action = memories[1][idx,:]
-        reward = memories[2][idx,:]
-        next_state = memories[3][idx,:]
+    #data_size = 100000
+    for i in range(memories[0].shape[0]):
+        #idx = int(np.floor(np.random.random()*memories[0].shape[0]))
+        state = memories[0][i,:]
+        action = memories[1][i,:]
+        reward = memories[2][i,:]
+        next_state = memories[3][i,:]
         
         train_data.append(np.concatenate((state, action, reward, next_state)))
     
-    
-    train_data_tf = tf.data.Dataset.from_tensor_slices(train_data).batch(batch_size)
+    # could shuffle here. I'm unclear on randomizing each step or maintaining order
+    train_data_tf = tf.data.Dataset.from_tensor_slices(train_data).shuffle(50000).batch(batch_size)
+    #train_data_tf = tf.data.Dataset.from_tensor_slices(train_data).batch(batch_size)
     
     for epoch in range(epochs):
         # Reset the metrics at the start of the next epoch
         train_loss.reset_states()
         train(train_data_tf)
         #print("works")
-        template = 'Epoch {}, Loss: {}'
+        template = '\nEpoch {}, Loss: {}\n'
         print(template.format(epoch + 1, train_loss.result()))
+        
+        #updates target network
+        if epoch % 1000 == 0:
+            prev_model = curr_model
 
+    curr_model.summary()
+    #Saves model
+    curr_model.save_weights('./trained_weights/foosPong_model_v0')
     
     
 ###########################################################################
@@ -145,6 +164,10 @@ with open("memory_next_states.txt", "rb") as fp:
     memory_next_states = np.asarray(pickle.load(fp), dtype='float32')
 
 memories = [memory_states, memory_actions, memory_rewards, memory_next_states]
+
+
+###########################################################################
+# This function starts it all. Set hasPrevModel=False for first trainging attempt
 
 train_nn(memories, hasPrevModel=False)
 
